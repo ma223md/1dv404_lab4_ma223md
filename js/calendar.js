f22f06b56987a296dcdf1935df4fabe15a5d4b2d
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-18 11:44:46 +0000

    Structure for classes and test file

--- js/calendar.js
+++ js/calendar.js
@@ -3,19 +3,24 @@
 function Calendar(){
     // array with dates
     var dateArray = [];
-    
-    Calendar.prototype.isDateFree = function(date) {
-        // check date array if date is free
-        // if not return false
-        // else return true
-        return true;
-    };
-    
-    Calendar.prototype.freeDate = function(date) {
-        // remove date from list
-    }
-    
-    Calendar.prototype.setDate = function(date) {
-        // add date to list
-    }
 }
+
+Calendar.prototype.isDateFree = function(date) {
+    // check date array if date is free
+    for (var i = 0; i < this.dateArray.length; i++) {
+        if (this.dateArray[i] == date) {
+            return false;
+        }
+    }
+    // else return true
+    return true;
+};
+
+Calendar.prototype.freeDate = function(date) {
+    // remove date from list
+};
+
+Calendar.prototype.setDate = function(date) {
+    // add date to list
+    this.dateArray.push(date);
+};
--- js/contest.js
+++ js/contest.js
@@ -4,19 +4,22 @@ function Contest(date, text){
     // infotext
     this.getText = function() {
         return text;
-    }
+    };
     
     this.setText = function(_text) {
-        text = _text
-    }
+        text = _text;
+    };
     
+    // date
     this.getDate = function() {
         return date;
-    }
+    };
     
     this.setDate = function(_date) {
         date = _date;
-    }
-    
-    // date
+    };
 }
+
+Contest.prototype.toString = function() {
+    return this.getText() + " (" + this.getDate() + ")";
+};
