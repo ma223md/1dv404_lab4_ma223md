30d9668e72dc70e86fd0559e7de0f62712035797
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-19 23:34:01 +0000

    First tests working

--- js/test_run.js
+++ js/test_run.js
@@ -4,6 +4,28 @@ function isEven(val) {
     return val % 2 === 0;
 }
 
+
+
+var d = new Date("December 20, 2014"),
+otherd = new Date("December 21, 2014"),
+dateArray = [];
+
+dateArray.push(d);
+
+function isDateFree(date) {
+    // check date array if date is free
+    for (var i = 0; i < dateArray.length; i++) {
+        if (dateArray[i] === date) {
+            return false;
+        }
+    }
+}
+    
+test (isDateFree(), function(){
+    ok(isDateFree(otherd), 'Date is free');
+    ok(!isDateFree(d), 'Date is not free');
+}) 
+
 test (isEven(), function() {
     ok(isEven(0), 'Zero is an even number');
     ok(isEven(2), 'So is two');
